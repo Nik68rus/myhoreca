d0b760579890f2282c612feb06b367af9d4ffeff
Nik68rus <nik200x@mail.ru>
2023-01-25 20:04:32 +0300

    deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,29 +8,15 @@ module.exports = {
   deploy: {
     production: {
       user: 'root',
-      host: '62.113.111.18',
+      host: '185.225.35.163',
       ref: 'origin/main',
       repo: 'git@github.com:Nik68rus/myhoreca.git',
       path: '/home/root',
       'pre-deploy-local': '',
       'post-deploy':
-        'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+        'npm install && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
       ssh_options: 'ForwardAgent=yes',
-      env: {
-        DB_NAME: process.env.DB_NAME,
-        DB_USER: process.env.DB_USER,
-        DB_PASSWORD: process.env.DB_PASSWORD,
-        DB_HOST: process.env.DB_HOST,
-        DB_PORT: process.env.DB_PORT,
-        JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
-        JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
-        MAIL_USER: process.env.MAIL_USER,
-        MAIL_PASSWORD: process.env.MAIL_PASSWORD,
-        MAIL_HOST: process.env.MAIL_HOST,
-        MAIL_PORT: process.env.MAIL_PORT,
-        APP_URL: process.env.APP_URL,
-      },
     },
   },
 };
