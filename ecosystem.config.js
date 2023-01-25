0739cbd6f20f2876ec0f9f9c2cda865ca514aa1f
Nik68rus <nik200x@mail.ru>
2023-01-25 20:13:06 +0300

    pm2 ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,13 +8,13 @@ module.exports = {
   deploy: {
     production: {
       user: 'root',
-      host: '185.225.35.163',
+      host: '45.130.43.140',
       ref: 'origin/main',
       repo: 'git@github.com:Nik68rus/myhoreca.git',
       path: '/home/root',
       'pre-deploy-local': '',
       'post-deploy':
-        'npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+        'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
       ssh_options: 'ForwardAgent=yes',
     },
