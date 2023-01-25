7868fdda0c28c226837a92661c5ef134a0401706
Nik68rus <nik200x@mail.ru>
2023-01-25 16:29:17 +0300

    env var pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,20 @@ module.exports = {
   apps: [
     {
       script: 'npm start',
+      env: {
+        DB_NAME: process.env.DB_NAME,
+        DB_USER: process.env.DB_USER,
+        DB_PASSWORD: process.env.DB_PASSWORD,
+        DB_HOST: process.env.DB_HOST,
+        DB_PORT: process.env.DB_PORT,
+        JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
+        JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
+        MAIL_USER: process.env.MAIL_USER,
+        MAIL_PASSWORD: process.env.MAIL_PASSWORD,
+        MAIL_HOST: process.env.MAIL_HOST,
+        MAIL_PORT: process.env.MAIL_PORT,
+        APP_URL: process.env.APP_URL,
+      },
     },
   ],
 
