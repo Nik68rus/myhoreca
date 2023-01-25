76c217a16095c184bf5eb2b1888de58825d72c0c
Nik68rus <nik200x@mail.ru>
2023-01-25 14:02:31 +0300

    ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      script: 'npm start',
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: 'root',
+      host: '62.113.111.18',
+      ref: 'origin/main',
+      repo: 'git@github.com:Nik68rus/myhoreca.git',
+      path: '/home/root',
+      'pre-deploy-local': '',
+      'post-deploy':
+        'source ~/nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      ssh_options: 'ForwardAgent=yes',
+    },
+  },
+};
